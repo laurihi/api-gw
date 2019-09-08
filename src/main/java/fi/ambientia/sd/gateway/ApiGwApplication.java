007d9572e0e092a4f1cec3186a48b3904a09e5e2
Lauri Hiltunen <hiltusenlauri@gmail.com>
2019-09-08 15:31:50 +0300

    Setup discovery client, add actuator with default info

--- src/main/java/fi/ambientia/sd/gateway/ApiGwApplication.java
+++ src/main/java/fi/ambientia/sd/gateway/ApiGwApplication.java
@@ -2,8 +2,10 @@ package fi.ambientia.sd.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGwApplication {
 
 	public static void main(String[] args) {
