b0c19eca76e33b60d74e269ef9238e5b7cfb2d50
Lauri Hiltunen <hiltusenlauri@gmail.com>
2019-09-10 20:33:11 +0300

    Add webflux conversion service and enable zuul proxy

--- src/main/java/fi/ambientia/sd/gateway/ApiGwApplication.java
+++ src/main/java/fi/ambientia/sd/gateway/ApiGwApplication.java
@@ -1,15 +1,27 @@
 package fi.ambientia.sd.gateway;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.format.WebConversionService;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.support.DefaultConversionService;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZuulProxy
 public class ApiGwApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGwApplication.class, args);
 	}
 
+	@Bean("webFluxConversionService")
+	public ConversionService conversionService() {
+		return new WebConversionService("");
+	}
 }
